d7ea9a6338d13122c656ba08cf8154ef595946cf
glitchoutguy <aarontrekell@outlook.com>
2025-08-26 14:14:10 -0700

    made platform independent executable

---
+++ target/x86_64-unknown-linux-musl/release/deps/elastic_importer-45ba9251f3d4ce94.d
@@ -0,0 +1,5 @@
+/home/aaron/projects/elastic_importer/elastic_importer/target/x86_64-unknown-linux-musl/release/deps/elastic_importer-45ba9251f3d4ce94.d: src/main.rs
+
+/home/aaron/projects/elastic_importer/elastic_importer/target/x86_64-unknown-linux-musl/release/deps/elastic_importer-45ba9251f3d4ce94: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-unknown-linux-musl/release/elastic_importer.d
@@ -0,0 +1,1 @@
+/home/aaron/projects/elastic_importer/elastic_importer/target/x86_64-unknown-linux-musl/release/elastic_importer: /home/aaron/projects/elastic_importer/elastic_importer/src/main.rs
